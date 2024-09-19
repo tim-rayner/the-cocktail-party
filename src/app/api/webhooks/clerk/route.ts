439d5cd9d4e8646672804a81cf61770eca39ea44
tim-rayner <tim.rayner2020@gmail.com>
2024-09-19 23:06:47 +0100

    fix: fixed typo in webhook function

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -5,7 +5,7 @@ import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
-import { createUser } from "@/app/lib/actions/user.action";
+import { createUser } from "@/app/lib/actions/createUser.action";
 import User from "@/models/User";
 
 export async function POST(req: Request) {
